cab7e9c6a346026c1153ab0331c57892701be882
Oleksandr Maniukhin <133182187+sandrinus@users.noreply.github.com>
2025-08-03 20:18:59 -0400

    Update wheel_of_fortune.py

--- casino/wheel_of_fortune.py
+++ casino/wheel_of_fortune.py
@@ -71,7 +71,7 @@ async def spin_wheel_logic(interaction: discord.Interaction, bet=1000, view=None
 
         if result == "x2":
             new_amount = bal * 2
-            await update_balance(uid, new_amount, bal)
+            await update_balance(uid, new_amount,0)
             msg_text = f"✨ You hit `x2`! Your balance is doubled! Now you have **{new_amount}** coins!"
         elif result == "/2":
             if bal % 2 != 0:
@@ -81,8 +81,12 @@ async def spin_wheel_logic(interaction: discord.Interaction, bet=1000, view=None
             msg_text = f"➗ You hit `/2`! You lose half your coins: **-{bal - new_amount}** coins."
         else:
             new_amount = bal + result
-            await update_balance(uid, new_amount, result)
-            outcome = "won" if result > 0 else "lost"
+            if result > 0:
+                await update_balance(uid, new_amount, 0)
+                outcome = "won"
+            else:
+                await update_balance(uid, new_amount, result)
+                outcome = "lost"
             msg_text = f"You {outcome} **{abs(result)}** coins!"
 
     final_embed = embed_wheel(final_index)
