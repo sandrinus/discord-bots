bb2f901ef992ac2bfe448c9affceeccfd47c7da1
Oleksandr Maniukhin <133182187+sandrinus@users.noreply.github.com>
2025-08-16 21:16:36 -0400

    Update main.py

--- casino/main.py
+++ casino/main.py
@@ -4,7 +4,7 @@ import time
 import os
 from datetime import datetime
 from database import init_pool, get_pool, init_db, get_balance, update_balance, get_user_lock
-from slots import SlotView
+from slots import SlotView, orig_msg
 from blackjack import BlackjackBetView
 from wheel_of_fortune import FortuneView, embed_wheel, get_wheel_state
 
@@ -62,6 +62,7 @@ class CasinoHomeView(discord.ui.View):
             view=SlotView(),
             ephemeral=True
         )
+        orig_msg = await interaction.original_response()
 
     @discord.ui.button(label="🃏 Go to Blackjack", style=discord.ButtonStyle.success, custom_id="goto_blackjack")
     async def goto_blackjack(self, interaction: discord.Interaction, button: discord.ui.Button):
