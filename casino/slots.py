edebb40e2904fde8b64d1ce08c306243fb9c136a
Oleksandr Maniukhin <133182187+sandrinus@users.noreply.github.com>
2025-08-23 16:17:18 -0400

    Revise slot symbols distribution

--- casino/slots.py
+++ casino/slots.py
@@ -4,8 +4,8 @@ import asyncio
 from database import get_balance, update_balance_atomic, can_act
 
 SLOT_SYMBOLS = (
-    ["🍒"] * 11 + ["🍋"] * 8 + ["🍉"] * 6 +
-    ["🍇"] * 4 + ["🔔"] * 3 + ["🍀"] * 2
+    ["🍒"] * 15 + ["🍋"] * 13 + ["🍉"] * 9 +
+    ["🍇"] * 7 + ["🔔"] * 5 + ["🍀"] * 3
 )
 SYMBOL_COEFFICIENTS = {
     "🍒": 2, "🍋": 3, "🍉": 4,
